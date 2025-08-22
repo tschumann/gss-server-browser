4de029463bc3facf319c1f8495e4731fb8dbe9db
tschumann <schumann.tom@gmail.com>
2025-08-22 15:06:14 +1000

    Added GameServerList.Common tests.

---
+++ GameServerList.Common.Test/Utils/StringUtilsTest.cs
@@ -0,0 +1,18 @@
+﻿using GameServerList.Common.Utils;
+
+namespace GameServerList.Common.Test.Utils
+{
+    [TestClass]
+    public sealed class StringUtilsTest
+    {
+        [TestMethod]
+        public void TestTruncate()
+        {
+            Assert.AreEqual("", StringUtils.Truncate(null, 0));
+            Assert.AreEqual("…", StringUtils.Truncate(" ", 0));
+            Assert.AreEqual("", StringUtils.Truncate("", 0));
+            Assert.AreEqual("…", StringUtils.Truncate("hello", 0));
+            Assert.AreEqual("he…", StringUtils.Truncate("hello", 2));
+        }
+    }
+}
